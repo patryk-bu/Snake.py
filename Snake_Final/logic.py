ac0b9d75892bf93ddc0d5586602bd667f2fde3f6
Patryk Buczek <46571803+patryk-bu@users.noreply.github.com>
2020-10-12 15:38:15 +0100

    update

--- Snake_Final/logic.py
+++ Snake_Final/logic.py
@@ -1,9 +1,12 @@
 import random
 import keyboard
 
-#snake parts
+
+# class for each individual snake body part
 class snake_parts:
+    # constructor and attributes
     def __init__(self, head, x, y):
+        # part can be head or not
         if head:
             self.head = True
         else:
@@ -11,6 +14,7 @@ class snake_parts:
         self.x = x
         self.y = y
 
+    # methods
     def get_x(self):
         return self.x
 
@@ -27,14 +31,21 @@ class snake_parts:
         self.y = y
 
 
+# class for snake object
 class snake:
     def __init__(self):
+        # object is made up of snake_parts objects
         self.snake_parts_list = []
         self.direction = "n"
+        # creates head at starting position (9,9)
         self.snake_parts_list.append(snake_parts(True, 9, 9))
 
-
+    # methods
+    # movement method (decides movement of snake as a whole)
     def movement(self):
+        # using temp list we set the next position of each part to the previous position of snake part before it
+        # so [head,body1,body2] the position of body2 would be set to body1, body1 to head and head position is
+        # calculated  in the next part
         temp = self.snake_parts_list
         tempx = []
         tempy = []
@@ -44,9 +55,11 @@ class snake:
         for i in range(1, len(temp)):
             self.snake_parts_list[i].set_y(tempy[i - 1])
             self.snake_parts_list[i].set_x(tempx[i - 1])
-
+        # the next position of the head is decided by the value of the direction attribute
+        # if up is pressed then the snake will move 'n' or up until another key is pressed
         if self.direction == "n":
             self.snake_parts_list[0].set_y(int(self.snake_parts_list[0].get_y()) - 1)
+            # allows snake to wraparound play area
             if self.snake_parts_list[0].get_y() == -1:
                 self.snake_parts_list[0].set_y(19)
         elif self.direction == "e":
@@ -62,19 +75,27 @@ class snake:
             if self.snake_parts_list[0].get_x() == -1:
                 self.snake_parts_list[0].set_x(19)
 
+    # grow method
     def grow(self):
-        end = len(self.snake_parts_list)-1
+        # finds position of end piece and adds piece after it when next movement is made
+        end = len(self.snake_parts_list) - 1
         x = self.snake_parts_list[end].get_x()
         y = self.snake_parts_list[end].get_y()
         self.snake_parts_list.append(snake_parts(False, x, y))
 
     def set_direction(self, dir):
+        # sets direction to passed parameter
         self.direction = dir
 
+    # keyboard control method
     def control(self, prev_dir):
+        # based on what key is pressed the snake moves N or E or S or W
         if keyboard.is_pressed("up"):
+            # the snake cant move back, only forwards left or right
             if self.direction != "s":
                 return "n"
+            # if the user tries moving the head into the part behind i.e presses up whilst the snake is directed south
+            # south is returned as the user has made an invalid move
             else:
                 return prev_dir
         elif keyboard.is_pressed("down"):
@@ -96,16 +117,18 @@ class snake:
             return prev_dir
 
 
+# apple class
 class apple:
+    # constructor initialises random x and y coordinates in play area for apple to appear
     def __init__(self):
         self.x = random.randint(0, 19)
         self.y = random.randint(0, 19)
 
+    # method for new position
     def new_pos(self, snakes):
+        # if random coordinates are the same as coordinates of a snake part the new coordinates are generated, recursive
         for i in snakes:
             if (i.get_x(), i.get_y()) == (self.x, self.y):
                 self.x = random.randint(0, 19)
                 self.y = random.randint(0, 19)
                 self.new_pos(snakes)
-
-
--- Snake_Final/main.py
+++ Snake_Final/main.py
@@ -1,15 +1,20 @@
+# import needed modules
 import tkinter as tk
 from tkinter import messagebox
 import logic as log
 import sys
 
+# creates snake and apple object
 s = log.snake()
 a = log.apple()
 
 
+# main app class
 class App(tk.Tk):
+    # creates tkinter window
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        # creates a canvas on which game is displayed
         self.canvas = tk.Canvas(self, width=500, height=500, borderwidth=0, highlightthickness=0)
         self.canvas.pack(side="top", fill="both", expand="true")
         self.cell_width = 25
@@ -17,6 +22,7 @@ class App(tk.Tk):
         self.rect = {}
         self.oval = {}
         self.turns = 0
+        # creates grid needed for displaying snake and apple
         for column in range(20):
             for row in range(20):
                 x1 = column * self.cell_width
@@ -27,51 +33,70 @@ class App(tk.Tk):
                 self.oval[row, column] = self.canvas.create_oval(x1 + 2, y1 + 2, x2 - 2, y2 - 2, fill="black",
                                                                  tags="oval")
                 self.canvas.itemconfig(self.oval[row, column], state="hidden")
+        # new position for apple is generated
         a.new_pos(s.snake_parts_list)
+        # snake is given head and 2 body parts to start with
         s.grow()
         s.grow()
-        self.re_draw(150)
+        # set speed at which game runs, delay of 100ms before updating
+        self.re_draw(100)
 
+    # class for drawing snake and apple positions
     def re_draw(self, delay):
+        # sets every rectangle to black to start with
         for y in range(0, 20):
             for x in range(0, 20):
                 self.canvas.itemconfig(self.rect[y, x], fill="black")
+        # runs game play function, sets new snake position
         self.game_play()
+        # sets the oval at specified coordinates to show an apple based on apple objects coordinates
         self.canvas.itemconfig(self.oval[a.y, a.x], state="normal", fill="green")
+        # draws orange rectangle for head and red  for body part, loops through each part
         to_draw = []
-        x = 0
         for i in s.snake_parts_list:
-            to_draw.append((self.rect[i.get_y(), i.get_x()], i.is_head(), x))
-            x += 1
+            to_draw.append((self.rect[i.get_y(), i.get_x()], i.is_head()))
         for i in to_draw:
             if i[1]:
                 self.canvas.itemconfig(i[0], fill="orange")
             else:
                 self.canvas.itemconfig(i[0], fill="red")
+        # recursively runs re_draw method after specified delay in ms
         self.after(delay, lambda: self.re_draw(delay))
 
+    # game play method
     def game_play(self):
+        # due to bug of the game registering a death at the start the turns attributes is used
         self.turns += 1
         head_x = s.snake_parts_list[0].get_x()
         head_y = s.snake_parts_list[0].get_y()
+        # if at any point the head coordinates are equal to apple coordinates the snake grows and new apple position
+        # is generated
         if (head_x, head_y) == (a.x, a.y):
             self.canvas.itemconfig(self.oval[a.y, a.x], state="hidden", fill="black")
             a.new_pos(s.snake_parts_list)
             s.grow()
+        # checks through every body part of snake if the head and body part have equal coordinates
         for i in range(1, len(s.snake_parts_list)):
             if self.turns > 1:
                 if (s.snake_parts_list[i].get_x(), s.snake_parts_list[i].get_y()) == (head_x, head_y):
+                    # if yes the game is over as the snake has died and the exit_application method runs
                     self.exit_application()
                     break
+        # direction is determined
         s.direction = s.control(s.direction)
+        # snake moves with movement method
         s.movement()
 
     def exit_application(self):
+        # displays ask question message box with the answers yes or no
+        # score is displayed and is user is asked is they want to play again
         msg_box = messagebox.askquestion("Game Over",
                                          'Do you want to play again?' + '\n' + (
                                                  "You Scored: " + str(len(s.snake_parts_list) - 1)),
                                          icon='warning')
+        # if they choose to play again
         if msg_box == 'yes':
+            # all variables and lists used are reset to their defaults and the game starts from the beginning
             self.turns = 0
             self.deiconify()
             s.snake_parts_list = []
@@ -83,6 +108,7 @@ class App(tk.Tk):
             a.new_pos(s.snake_parts_list)
 
         else:
+            # otherwise the script closes down
             sys.exit()
 
 
